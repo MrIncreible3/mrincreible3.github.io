7de23e11ebb698c3c9277831f30eee61ce405dba
mrincreible <mrincreible@mrincreible.mrincreible>
2025-10-27 17:18:14 +0100

    HTB-Imagery

--- _site/sw.js
+++ _site/sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-1761171361';
+const cacheName = 'chirpy-1761581853';
 
 function verifyDomain(url) {
   for (const domain of allowedDomains) {
